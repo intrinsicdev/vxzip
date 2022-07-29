a4499ce50e81820171dbd2fe6cc48612855b7fa6
Tom <intrinsic.dev@outlook.com>
2022-07-29 11:30:09 -0400

    Add commandline parsing.

--- src/vxzip/vxzip.cpp
+++ src/vxzip/vxzip.cpp
@@ -7,6 +7,28 @@
  *********************************************************************/
 #include "vxzip.h"
 
+ /**
+  * Redirection of the log messages to signal failures.
+  *
+  * ex: SPEW_ASSERT will return SPEW_DEBUGGER to log this message to the debugger as well.
+  *
+  * \param spewType Message spew type
+  * \param pMsg
+  * \return Spew return value
+  */
+static SpewRetval_t OutputFunc(SpewType_t spewType, char const* pMsg)
+{
+	// write to standard output
+	printf(pMsg);
+
+	switch (spewType)
+	{
+	case SPEW_ERROR: return SPEW_ABORT;
+	case SPEW_ASSERT:return SPEW_DEBUGGER;
+	default: return SPEW_CONTINUE;
+	}
+}
+
 bool CVXZipApp::Create()
 {
 	// Redirect spew output
@@ -31,6 +53,34 @@ bool CVXZipApp::PreInit()
 
 int CVXZipApp::Main()
 {
+	if (!ParseCommandLine())
+	{
+		PrintHelp();
+		return 1;
+	}
+
+	auto idxBuildParam = CommandLine()->FindParm(this->m_szBuildToken);
+	auto idxExtractParam = CommandLine()->FindParm(this->m_szExtractToken);
+	auto idxTargetParam = CommandLine()->FindParm(this->m_szTargetToken);
+
+	auto paramTarget = CUtlString(CommandLine()->GetParm(idxTargetParam + 1));
+	auto paramAction = CUtlString();
+
+	if (idxBuildParam)
+	{
+		// build xzip
+		paramAction.Set(CommandLine()->GetParm(idxBuildParam + 1));
+		return this->BuildXZip(paramTarget, paramAction) ? 1 : 0;
+	}
+	else
+	{
+		// extract xzip
+		paramAction.Set(CommandLine()->GetParm(idxExtractParam + 1));
+
+		return this->ExtractXZip(paramAction, paramTarget) ? 1 : 0;
+	}
+
+	// success.
 	return 0;
 }
 
@@ -44,4 +94,55 @@ void CVXZipApp::PostShutdown()
 	DisconnectTier2Libraries();
 	DisconnectTier1Libraries();
 	return;
+}
+
+void CVXZipApp::PrintHelp()
+{
+	Msg("vxzip - Valve Source Engine Console Game Asset Builder\n");
+	Msg("by Intrinsic <intrinsic.dev@outlook.com>\n(build: %s %s)\n", __DATE__, __TIME__);
+	Msg("\n");
+
+	Msg("Usage:\n");
+	Msg("\tvxzip.exe [options]\n");
+
+	Msg("\n");
+	Msg("Options:\n");
+	Msg("\t%s [input folder]            Build pak file(s)\n", this->m_szBuildToken);
+	Msg("\t%s [input zip]               Extract pak file\n", this->m_szExtractToken);
+	Msg("\t%s [target zip or folder]    Target zip filename or output folder\n", this->m_szTargetToken);
+	Msg("\n");
+}
+
+bool CVXZipApp::ParseCommandLine()
+{
+	auto idxBuildArg = CommandLine()->FindParm(this->m_szBuildToken);
+	auto idxExtractArg = CommandLine()->FindParm(this->m_szExtractToken);
+	auto idxTargetArg = CommandLine()->FindParm(this->m_szTargetToken);
+
+	if (!idxTargetArg ||					// target parameter is always required
+		(!idxBuildArg && !idxExtractArg) ||	// we need a build/extract parameter
+		(idxBuildArg && idxExtractArg)		// cannot do both
+		)
+	{
+		Error("Invalid parameter(s) provided.\n");
+		return false;
+	}
+
+	return true;
+}
+
+bool CVXZipApp::ExtractXZip(CUtlString& outputPath, CUtlString& zipPath)
+{
+	Msg("Extracting '%s' to '%s'", zipPath.Get(), outputPath.Get());
+
+	// success
+	return true;
+}
+
+bool CVXZipApp::BuildXZip(CUtlString& inputPath, CUtlString& zipPath)
+{
+	Msg("Building '%s' from '%s'", zipPath.Get(), inputPath.Get());
+
+	// success
+	return true;
 }
--- src/vxzip/vxzip.h
+++ src/vxzip/vxzip.h
@@ -8,6 +8,7 @@
 #pragma once
 
 #include <appframework/appframework.h>
+#include <tier0/icommandline.h>
 #include <tier1/tier1.h>
 #include <tier2/tier2.h>
 #include "xzip_file.h"
@@ -47,32 +48,44 @@ public:
 	 * Contains the zip file handles held by this application.
 	 */
 	CUtlLinkedList< HANDLE > m_ZipFileHandles;
+
+public:
+	/**
+	 * Spews app help and usage.
+	 *
+	 */
+	void PrintHelp();
+	/**
+	 * Parses the commandline options into the application object.
+	 *
+	 * \return True indicates success
+	 */
+	bool ParseCommandLine();
+	/**
+	 * Extracts an xzip pak file to the file system.
+	 *
+	 * \param outputPath	Output directory pak files
+	 * \param zipPath		Input path for xzip pak
+	 * \return True indicates success
+	 */
+	bool ExtractXZip(CUtlString& outputPath, CUtlString& zipPath);
+	/**
+	 * Constructs an xzip pak from a given directory.
+	 *
+	 * \param inputPath		Input directory to pack
+	 * \param zipPath		Output path for xzip pak
+	 * \return True indicates success
+	 */
+	bool BuildXZip(CUtlString& inputPath, CUtlString& zipPath);
+
+private:
+	// parameter tokens
+	const char* m_szTargetToken = "-t";
+	const char* m_szExtractToken = "-e";
+	const char* m_szBuildToken = "-b";
 };
 
 /**
  * Register the application.
  */
 DEFINE_CONSOLE_APPLICATION_OBJECT(CVXZipApp);
-
-/**
- * Redirection of the log messages to signal failures.
- *
- * ex: SPEW_ASSERT will return SPEW_DEBUGGER to log this message to the debugger as well.
- *
- * \param spewType Message spew type
- * \param pMsg
- * \return Spew return value
- */
-static SpewRetval_t OutputFunc(SpewType_t spewType, char const* pMsg)
-{
-	// write to standard output
-	printf(pMsg);
-
-	// determine our spew result
-	switch (spewType)
-	{
-	case SPEW_ERROR: return SPEW_ABORT;
-	case SPEW_ASSERT:return SPEW_DEBUGGER;
-	default: return SPEW_CONTINUE;
-	}
-}
